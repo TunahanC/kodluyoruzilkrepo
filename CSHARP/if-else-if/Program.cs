225346b22cd77e0a1a4fb1e773df6c85a07831e5
Tunahan <ctunahan3@gmail.com>
2023-01-17 01:14:45 +0300

    ifelseif

---
+++ CSHARP/if-else-if/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int time = DateTime.Now.Hour;
+
+if (time>=6 && time<11)
+    Console.WriteLine("Günaydin!");
+else if(time<=18)
+    Console.WriteLine("iyi günler!");
+else
+    Console.WriteLine("iyi geceler!");
+
+string sonuc = time<=10 ? "iyi günler!" : "iyi geceler";
+
+sonuc = time>=6 && time<11 ? "günaydin!" : time<=18 ? "iyi günler!" :"iyi geceler";
+Console.WriteLine(sonuc);
