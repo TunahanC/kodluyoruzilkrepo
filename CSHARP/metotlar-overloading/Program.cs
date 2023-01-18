b35d16368c6e8a67e44dcc615d39df875428cbed
Tunahan <ctunahan3@gmail.com>
2023-01-18 14:53:19 +0300

    metotlar-overloading

---
+++ CSHARP/metotlar-overloading/Program.cs
@@ -0,0 +1,56 @@
+﻿   //out parametreler
+   string sayi ="999";
+
+   bool sonuc = int.TryParse(sayi, out int outSayi);
+   if (sonuc)
+   {
+
+    Console.WriteLine("Başarili!");
+    Console.WriteLine(outSayi);
+   }
+
+   else 
+   {
+    Console.WriteLine("Başarisiz!");
+   }
+
+   Metotlar instance = new Metotlar();
+   instance.Topla(4,5, out int toplamSonucu);
+   Console.WriteLine(toplamSonucu);
+
+
+   //Metot Aşırı Yükleme - Overloading
+   int ifade = 999;
+   instance.EkranaYazdir (Convert.ToString(ifade));
+   instance.EkranaYazdir (ifade);
+   instance.EkranaYazdir ("Zikriye", "Ürkmez");
+
+
+   //Metot Imzası
+   //metotAdi + parametre sayisi + parametre
+
+class Metotlar
+{
+
+   public void Topla(int a, int b, out int toplam)
+   {
+      toplam = a+b;
+   }
+
+   public void EkranaYazdir(string veri)
+   {
+      Console.WriteLine(veri);
+   }
+
+   public void EkranaYazdir(int veri)
+   {
+      Console.WriteLine(veri);
+   }
+
+   public void EkranaYazdir(string veril, string veri2)
+   {
+      Console.WriteLine(veril + veri2);
+   }
+
+}
+
